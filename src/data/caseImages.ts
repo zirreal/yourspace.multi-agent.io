9f13afc836d063f5cbb10d3187b51edf23b1e692
Julia <uug11@mail.ru>
2025-07-30 20:06:33 +0400

    added cases images

---
+++ src/data/caseImages.ts
@@ -0,0 +1,52 @@
+import CASE1 from '../assets/img/cases/1.webp';
+import CASE2 from '../assets/img/cases/2.webp';
+import CASE3 from '../assets/img/cases/3.webp';
+import CASE4 from '../assets/img/cases/4.webp';
+import CASE5 from '../assets/img/cases/5.webp';
+import CASE6 from '../assets/img/cases/6.webp';
+import CASE7 from '../assets/img/cases/7.webp';
+import CASE8 from '../assets/img/cases/8.webp';
+import CASE9 from '../assets/img/cases/9.webp';
+import CASE10 from '../assets/img/cases/10.webp';
+import CASE11 from '../assets/img/cases/11.webp';
+import CASE12 from '../assets/img/cases/12.webp';
+import CASE13 from '../assets/img/cases/13.webp';
+import CASE14 from '../assets/img/cases/14.webp';
+import CASE15 from '../assets/img/cases/15.webp';
+import CASE16 from '../assets/img/cases/16.webp';
+import CASE17 from '../assets/img/cases/17.webp';
+import CASE18 from '../assets/img/cases/18.webp';
+import CASE19 from '../assets/img/cases/19.webp';
+import CASE20 from '../assets/img/cases/20.webp';
+import CASE21 from '../assets/img/cases/21.webp';
+import CASE22 from '../assets/img/cases/22.webp';
+
+type CaseImage = {
+  src: ImageMetadata;
+  alt: string;
+};
+
+export const caseImages: CaseImage[] = [
+  { src: CASE1, alt: "Пример умного дома" },
+  { src: CASE2, alt: "Брошюра 1" },
+  { src: CASE3, alt: "Брошюра 2" },
+  { src: CASE4, alt: "Брошюра 3" },
+  { src: CASE5, alt: "Брошюра 4" },
+  { src: CASE6, alt: "Датчик почвы в земле" },
+  { src: CASE7, alt: "Датчик почвы" },
+  { src: CASE8, alt: "Датчик уровня в бочке" },
+  { src: CASE9, alt: "Датчик уровня" },
+  { src: CASE10, alt: "Кондиционер карточка" },
+  { src: CASE11, alt: "Кран полив" },
+  { src: CASE12, alt: "Параметр почвы" },
+  { src: CASE13, alt: "Терморегулятор карточка" },
+  { src: CASE14, alt: "Терморегулятор" },
+  { src: CASE15, alt: "Термостат" },
+  { src: CASE16, alt: "УД1" },
+  { src: CASE17, alt: "УД2" },
+  { src: CASE18, alt: "УД3" },
+  { src: CASE19, alt: "УД4-квадрат" },
+  { src: CASE20, alt: "УД4" },
+  { src: CASE21, alt: "УД5-квадрат" },
+  { src: CASE22, alt: "УД5" },
+];
