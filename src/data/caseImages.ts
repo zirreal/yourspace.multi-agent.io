bdaa5fab1f5a069a61a37441e232a9cf292fab19
Pavel Tarasov <p040399@outlook.com>
2025-07-30 20:16:33 +0300

    delete excess cases photos and edit policies

--- src/data/caseImages.ts
+++ src/data/caseImages.ts
@@ -11,15 +11,6 @@ import CASE10 from '../assets/img/cases/10.webp';
 import CASE11 from '../assets/img/cases/11.webp';
 import CASE12 from '../assets/img/cases/12.webp';
 import CASE13 from '../assets/img/cases/13.webp';
-import CASE14 from '../assets/img/cases/14.webp';
-import CASE15 from '../assets/img/cases/15.webp';
-import CASE16 from '../assets/img/cases/16.webp';
-import CASE17 from '../assets/img/cases/17.webp';
-import CASE18 from '../assets/img/cases/18.webp';
-import CASE19 from '../assets/img/cases/19.webp';
-import CASE20 from '../assets/img/cases/20.webp';
-import CASE21 from '../assets/img/cases/21.webp';
-import CASE22 from '../assets/img/cases/22.webp';
 
 type CaseImage = {
   src: ImageMetadata;
@@ -28,25 +19,16 @@ type CaseImage = {
 
 export const caseImages: CaseImage[] = [
   { src: CASE1, alt: "Пример умного дома" },
-  { src: CASE2, alt: "Брошюра 1" },
-  { src: CASE3, alt: "Брошюра 2" },
-  { src: CASE4, alt: "Брошюра 3" },
-  { src: CASE5, alt: "Брошюра 4" },
-  { src: CASE6, alt: "Датчик почвы в земле" },
-  { src: CASE7, alt: "Датчик почвы" },
-  { src: CASE8, alt: "Датчик уровня в бочке" },
-  { src: CASE9, alt: "Датчик уровня" },
-  { src: CASE10, alt: "Кондиционер карточка" },
-  { src: CASE11, alt: "Кран полив" },
-  { src: CASE12, alt: "Параметр почвы" },
-  { src: CASE13, alt: "Терморегулятор карточка" },
-  { src: CASE14, alt: "Терморегулятор" },
-  { src: CASE15, alt: "Термостат" },
-  { src: CASE16, alt: "УД1" },
-  { src: CASE17, alt: "УД2" },
-  { src: CASE18, alt: "УД3" },
-  { src: CASE19, alt: "УД4-квадрат" },
-  { src: CASE20, alt: "УД4" },
-  { src: CASE21, alt: "УД5-квадрат" },
-  { src: CASE22, alt: "УД5" },
+  { src: CASE2, alt: "Датчик почвы в земле" },
+  { src: CASE3, alt: "Датчик почвы" },
+  { src: CASE4, alt: "Датчик уровня в бочке" },
+  { src: CASE5, alt: "Датчик уровня" },
+  { src: CASE6, alt: "Кран полив" },
+  { src: CASE7, alt: "Параметр почвы" },
+  { src: CASE8, alt: "Терморегулятор карточка" },
+  { src: CASE9, alt: "Терморегулятор" },
+  { src: CASE10, alt: "Термостат" },
+  { src: CASE11, alt: "Протечка" },
+  { src: CASE12, alt: "Будильник" },
+  { src: CASE13, alt: "Кондиционер" },
 ];
